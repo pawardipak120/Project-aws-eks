4d4735fb60174c16467f9788d5789ed23863b351
Sunny Goswami <54610136+sunny7898@users.noreply.github.com>
2020-07-14 13:16:47 +0530

    First

--- Aws/aws_cluster.tf
+++ Aws/aws_cluster.tf
@@ -2,33 +2,6 @@ provider "aws" {
 	profile = "default"
 	region  = "ap-south-1"
 }
-/*
-		resource "aws_vpc" "main" {
-		  cidr_block       = "10.0.0.0/16"
-		  instance_tenancy = "default"
-
-		  tags = {
-			Name = "main"
-		  }
-		}
-
-		resource "aws_subnet" "example1" {
-		  vpc_id     = "${aws_vpc.main.id}"
-		  cidr_block = "10.0.1.0/24"
-
-		  tags = {
-			Name = "example1"
-		  }
-		}
-		resource "aws_subnet" "example2" {
-		  vpc_id     = "${aws_vpc.main.id}"
-		  cidr_block = "10.0.2.0/24"
-
-		  tags = {
-			Name = "example2"
-		  }
-		}
-*/
 
 resource "aws_iam_role" "example1" {
 	name = "eks-cluster-example"
